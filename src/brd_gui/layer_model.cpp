5f42ce37d963ed8e550b99b81992a96f46179c8f
Jeff Wheeler <jeffwheeler@gmail.com>
2024-06-09 23:40:28 -0700

    Show user-defined layer names in the layer selector GUI (#71)

--- src/brd_gui/layer_model.cpp
+++ src/brd_gui/layer_model.cpp
@@ -1,8 +1,70 @@
 #include "layer_model.h"
 
-LayerModel::LayerModel(const QString& data, QObject* parent)
+#include "lib/structure/utils.h"
+
+LayerModel::LayerModel(File<kAMax>& fs, QObject* parent)
     : QAbstractItemModel(parent),
       root_item_(std::make_unique<LayerItem>("Layer")) {
+  uint16_t i = 1;
+  for ([[maybe_unused]] const auto& [a, x2A_k] : fs.layers) {
+    uint8_t fixedEntriesLength = 0;
+
+    if (kFixedLayersMap.count(i) > 0) {
+      const Layer* fixedEntries = kFixedLayersMap.at(i);
+      while (fixedEntries[fixedEntriesLength].x != 0) fixedEntriesLength++;
+    }
+
+    uint8_t dynamicEntriesLength = 0;
+
+    if (x2A_k != 0x00 && fs.x2A_map.count(x2A_k) > 0) {
+      x2A* inst = &fs.x2A_map[x2A_k];
+      dynamicEntriesLength = inst->hdr.size;
+    }
+
+    Layer* layers = new Layer[dynamicEntriesLength + fixedEntriesLength + 1];
+    uint8_t currentLayer = 0;
+
+    if (x2A_k != 0x00 && fs.x2A_map.count(x2A_k) > 0) {
+      x2A* inst = &fs.x2A_map[x2A_k];
+      if (inst->references) {
+        for (const auto& entry : inst->reference_entries) {
+          layers[currentLayer].x = i;
+          layers[currentLayer].y = currentLayer;
+          layers[currentLayer].label = str_lookup(entry.ptr, fs);
+          currentLayer++;
+        }
+      } else {
+        for (const auto& entry : inst->local_entries) {
+          layers[currentLayer].x = i;
+          layers[currentLayer].y = currentLayer;
+          layers[currentLayer].label = entry.s.c_str();
+          currentLayer++;
+        }
+      }
+    }
+
+    // Now add the fixed layers
+    if (kFixedLayersMap.count(i) > 0) {
+      const Layer* fixedEntries = kFixedLayersMap.at(i);
+      for (uint8_t j = 0; j < fixedEntriesLength; j++) {
+        const Layer* layer = &fixedEntries[j];
+        layers[currentLayer].x = i;
+        layers[currentLayer].y = layer->y;
+        layers[currentLayer].label = layer->label;
+        currentLayer++;
+      }
+    }
+
+    // Add layer if we've iterated through _any_ layers, fixed or dynamic
+    if (currentLayer > 0) {
+      addLayerGroup(QString("%1 - ?").arg(i).toStdString(), layers);
+    }
+
+    delete[] layers;
+
+    i++;
+  }
+  /*
   addLayerGroup("1 - Board geometry?", kG1Layers);
   addLayerGroup("3 - ?", kG3Layers);
   addLayerGroup("4 - Drawing Format?", kG4Layers);
@@ -12,6 +74,7 @@ LayerModel::LayerModel(const QString& data, QObject* parent)
   addLayerGroup("C - ?", kGCLayers);
   addLayerGroup("D - Refdes?", kGDLayers);
   addLayerGroup("12 - ?", kG12Layers);
+  */
 };
 
 Qt::ItemFlags LayerModel::flags(const QModelIndex& index) const {
@@ -105,7 +168,7 @@ void LayerModel::addLayerGroup(const std::string label, const Layer layers[]) {
   uint16_t i = 0;
   while (true) {
     const Layer* layer_info = &layers[i];
-    if (layer_info->x == 0) break;
+    if (layer_info->x == 0 && layer_info->y == 0) break;
 
     const auto label_str = layer_info->label == nullptr
                                ? std::optional<std::string>()
--- src/brd_gui/layer_model.h
+++ src/brd_gui/layer_model.h
@@ -2,69 +2,41 @@
 #define FILM_MODEL_H
 
 #include <QtWidgets>
+#include <map>
 #include <set>
 
 #include "layer_item.h"
+#include "lib/structure/types.h"
 
 struct Layer {
-  const uint8_t x, y;
-  const char *label;
+  uint8_t x = 0, y = 0;
+  const char *label = nullptr;
 };
 
 const constexpr Layer kG1Layers[] = {
-    {0x1, 0x00, nullptr},    {0x1, 0x01, nullptr},
-    {0x1, 0x03, "Outline?"}, {0x1, 0x04, nullptr},
-    {0x1, 0x05, nullptr},    {0x1, 0x06, nullptr},
-    {0x1, 0x07, nullptr},    {0x1, 0x0F, nullptr},
-    {0x1, 0x10, nullptr},    {0x1, 0x11, nullptr},
-    {0x1, 0x14, nullptr},    {0x1, 0x16, nullptr},
-    {0x1, 0x19, nullptr},    {0x1, 0x23, nullptr},
-    {0x1, 0xEA, nullptr},    {0x1, 0xED, nullptr},
-    {0x1, 0xEE, nullptr},    {0x1, 0xF0, "Silk bot"},
-    {0x1, 0xF1, "Silk top"}, {0x1, 0xF9, "Fab dims?"},
-    {0x1, 0xFB, "Assy?"},    {0x1, 0xFD, "Design outline?"},
-    {0x1, 0xFF, "All"},      {0, 0, nullptr},
+    {0x01, 0xEA, nullptr},    {0x01, 0xED, nullptr},
+    {0x01, 0xEE, nullptr},    {0x01, 0xF0, "Silk bot"},
+    {0x01, 0xF1, "Silk top"}, {0x01, 0xF9, "Fab dims?"},
+    {0x01, 0xFB, "Assy?"},    {0x01, 0xFD, "Design outline?"},
+    {0, 0, nullptr},
 };
 
 const constexpr Layer kG3Layers[] = {
-    {0x3, 0x00, nullptr}, {0x3, 0x01, nullptr}, {0x3, 0xFA, nullptr},
-    {0x3, 0xFC, nullptr}, {0x3, 0xFD, nullptr}, {0x3, 0xFF, "All"},
+    {0x3, 0xFA, nullptr},
+    {0x3, 0xFC, nullptr},
+    {0x3, 0xFD, nullptr},
     {0, 0, nullptr},
 };
 
 const constexpr Layer kG4Layers[] = {
-    {0x04, 0x00, "Top something?"}, {0x04, 0x01, "Bottom something?"},
-    {0x04, 0x02, "Silk top"},       {0x04, 0x03, "Silk bot"},
-    {0x04, 0x04, "Mask top"},       {0x04, 0x05, "Mask bot"},
-    {0x04, 0x06, nullptr},          {0x04, 0x07, nullptr},
-    {0x04, 0x08, nullptr},          {0x04, 0x0B, nullptr},
-    {0x04, 0x0C, nullptr},          {0x04, 0x11, nullptr},
-    {0x04, 0x13, nullptr},          {0x04, 0x14, nullptr},
-    {0x04, 0x15, nullptr},          {0x04, 0x16, nullptr},
-    {0x04, 0xF9, nullptr},          {0x04, 0xFA, nullptr},
-    {0x04, 0xFB, nullptr},          {0x04, 0xFC, "Title block"},
-    {0x04, 0xFD, nullptr},          {0, 0, nullptr},
-};
-
-const constexpr Layer kG6Layers[] = {
-    {0x06, 0x00, nullptr}, {0x06, 0x01, nullptr}, {0x06, 0x02, nullptr},
-    {0x06, 0x03, nullptr}, {0x06, 0x04, nullptr}, {0x06, 0x05, nullptr},
-    {0x06, 0x06, nullptr}, {0x06, 0x07, nullptr}, {0x06, 0x08, nullptr},
-    {0x06, 0x09, nullptr}, {0x06, 0x0A, nullptr}, {0x06, 0x0B, nullptr},
-    {0x06, 0x0C, nullptr}, {0x06, 0x0D, nullptr}, {0x06, 0x0E, nullptr},
-    {0x06, 0x0F, nullptr}, {0, 0, nullptr},
+    {0x04, 0xF9, nullptr},       {0x04, 0xFA, nullptr}, {0x04, 0xFB, nullptr},
+    {0x04, 0xFC, "Title block"}, {0x04, 0xFD, nullptr}, {0, 0, nullptr},
 };
 
 const constexpr Layer kG7Layers[] = {
-    {0x07, 0x00, nullptr},        {0x07, 0x01, nullptr},
-    {0x07, 0x02, nullptr},        {0x07, 0x03, nullptr},
-    {0x07, 0x04, "Drill table?"}, {0x07, 0x06, "Drill table?"},
-    {0x07, 0x07, "Drill table?"}, {0x07, 0x09, nullptr},
-    {0x07, 0x0A, nullptr},        {0x07, 0x0B, nullptr},
-    {0x07, 0x0C, "Drill table?"}, {0x07, 0xF0, nullptr},
-    {0x07, 0x12, nullptr},        {0x07, 0x13, nullptr},
-    {0x07, 0xF7, nullptr},        {0x07, 0xF8, nullptr},
-    {0x07, 0xFD, nullptr},        {0, 0, nullptr},
+    {0x07, 0xF0, nullptr}, {0x07, 0x12, nullptr}, {0x07, 0x13, nullptr},
+    {0x07, 0xF7, nullptr}, {0x07, 0xF8, nullptr}, {0x07, 0xFD, nullptr},
+    {0, 0, nullptr},
 };
 
 const constexpr Layer kG9Layers[] = {
@@ -80,10 +52,6 @@ const constexpr Layer kG9Layers[] = {
 };
 
 const constexpr Layer kGCLayers[] = {
-    {0x0C, 0x00, nullptr}, {0x0C, 0x01, "L2"},    {0x0C, 0x02, "L3"},
-    {0x0C, 0x03, "L4"},    {0x0C, 0x04, "L5"},    {0x0C, 0x05, "L6"},
-    {0x0C, 0x06, "L7"},    {0x0C, 0x07, "L8"},    {0x0C, 0x08, "L9"},
-    {0x0C, 0x09, "L10"},   {0x0C, 0x0A, "L11"},   {0x0C, 0x0B, "L12"},
     {0x0C, 0xF4, nullptr}, {0x0C, 0xF5, nullptr}, {0x0C, 0xFA, nullptr},
     {0x0C, 0xFB, nullptr}, {0x0C, 0xFC, nullptr}, {0x0C, 0xFD, nullptr},
     {0, 0, nullptr},
@@ -97,18 +65,6 @@ const constexpr Layer kGDLayers[] = {
 };
 
 const constexpr Layer kG12Layers[] = {
-    {0x12, 0x00, "L1"},
-    {0x12, 0x01, "L2"},
-    {0x12, 0x02, "L3"},
-    {0x12, 0x03, "L4"},
-    {0x12, 0x04, "L5"},
-    {0x12, 0x05, "L6"},
-    {0x12, 0x06, "L7"},
-    {0x12, 0x07, "L8"},
-    {0x12, 0x08, "L9"},
-    {0x12, 0x09, "L10"},
-    {0x12, 0x0A, "L11"},
-    {0x12, 0x0B, "L12"},
     {0x12, 0xF4, nullptr},
     {0x12, 0xF5, nullptr},
     {0x12, 0xFA, "Paste bottom?"},
@@ -118,13 +74,18 @@ const constexpr Layer kG12Layers[] = {
     {0, 0, nullptr},
 };
 
+const std::map<uint16_t, const Layer *> kFixedLayersMap = {
+    {1, kG1Layers}, {3, kG3Layers},    {4, kG4Layers},    {7, kG7Layers},
+    {9, kG9Layers}, {0x0C, kGCLayers}, {0x0D, kGDLayers}, {0x12, kG12Layers},
+};
+
 class LayerModel : public QAbstractItemModel {
   Q_OBJECT
 
  public:
   Q_DISABLE_COPY_MOVE(LayerModel);
 
-  explicit LayerModel(const QString &data, QObject *parent = nullptr);
+  explicit LayerModel(File<kAMax> &data, QObject *parent = nullptr);
   ~LayerModel() override = default;
 
   Qt::ItemFlags flags(const QModelIndex &index) const override;
@@ -143,10 +104,10 @@ class LayerModel : public QAbstractItemModel {
   std::vector<QModelIndex> mapLayersToIndices(
       const std::set<std::pair<uint16_t, uint16_t>> &layer_pairs) const;
 
+  void addLayerGroup(const std::string label, const Layer layers[]);
+
  private:
   std::unique_ptr<LayerItem> root_item_;
-
-  void addLayerGroup(const std::string label, const Layer layers[]);
 };
 
 #endif
